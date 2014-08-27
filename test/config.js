ccfa084740b5cf3b16205c41b6b035ec8a675eca
Novtopro He <novtopro@gmail.com>
2014-08-27 22:42:49 +0800

    remove test/config.js

--- test/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  ACCESS_KEY: 'LPmVqPgRxrQcmvelW1udwkeTAMdA4Uz9IpT63x-7',
-  SECRET_KEY: 'oBSSxOCdvbuS2fccJSwiE-1-d1TmnoTQQvf9JYcs',
-  bucket_name: 'novtopro-test',
-  domain: 'http://novtopro-test.qiniudn.com'
-};
